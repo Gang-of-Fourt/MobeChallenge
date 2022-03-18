34ecc96928f35702b5b0c4b9b0793eced1234adc
paulinien <paulinien@live.fr>
2022-03-18 14:58:56 +0100

    Version 1 jeu vache

--- app/src/main/java/fr/gangoffourt/challengemobe/minigame/impl/CowGame.kt
+++ app/src/main/java/fr/gangoffourt/challengemobe/minigame/impl/CowGame.kt
@@ -4,28 +4,44 @@ import android.content.Context
 import android.graphics.*
 import android.hardware.Sensor
 import android.hardware.SensorEvent
+import android.util.Log
 import android.view.MotionEvent
 import com.example.challengemobe.R
 import fr.gangoffourt.challengemobe.GameView
 import fr.gangoffourt.challengemobe.minigame.AbstractMiniGame
 
-class CowGame (gameView: GameView, context: Context) : AbstractMiniGame (gameView, context) {
+class CowGame (private val gameView: GameView, context: Context) : AbstractMiniGame (gameView, context) {
 
     private var paint: Paint
     private val imgCow = context.getDrawable(R.drawable.vache)
     private val imgMilk = context.getDrawable(R.drawable.lait)
+    private val imgWater = context.getDrawable(R.drawable.eau)
+    private var isDone = false
+    private var isFailed = false
+    private var holdingMilk = false
+    private var holdingWater = false
+    private var xMilk = 0
+    private var yMilk = 0
+    private var xWater = 0
+    private var yWater = 0
+    private var textToDisplay = "J'ai soif !"
+
     init {
         paint = Paint()
     }
 
     override fun draw(canvas: Canvas) {
-        canvas.drawColor(Color.GREEN)
-        paint.color = Color.RED
+        canvas.drawColor(Color.BLACK)
+        paint.color = Color.WHITE
         val canvasBounds = canvas.clipBounds
         var left = canvasBounds.width()
         left /= 2
         var top = canvasBounds.height()
         top /= 2
+        paint.textSize = 70F;
+        paint.textAlign = Paint.Align.CENTER
+        canvas.drawText(textToDisplay, left.toFloat(), 90F, paint)
+
         if (imgCow != null) {
             left -= imgCow.intrinsicWidth.div(2)
             top -= imgCow.intrinsicHeight.div(2)
@@ -33,42 +49,96 @@ class CowGame (gameView: GameView, context: Context) : AbstractMiniGame (gameVie
             imgCow.draw(canvas)
         }
 
-        if (!holding) {
-            left = canvasBounds.width()
-            left /= 2
-            top = canvasBounds.height()
-        } else {
-            left = posX
-            top = posY
-        }
         if (imgMilk != null) {
+            if (!holdingMilk) {
+                if(!isFailed) {
+                    left = canvasBounds.width()
+                    left /= 2
+                    xMilk = left - imgMilk.intrinsicWidth/4
+                    top = canvasBounds.height()
+                    yMilk = top - imgMilk.intrinsicHeight/4
+                } else {
+                    left = xMilk+imgMilk.intrinsicWidth/8
+                    top = yMilk+imgMilk.intrinsicHeight/8
+                }
+            } else {
+                left = xMilk+imgMilk.intrinsicWidth/8
+                top = yMilk+imgMilk.intrinsicHeight/8
+            }
             left -= imgMilk.intrinsicWidth/4
             top -= imgMilk.intrinsicHeight/4
             imgMilk.setBounds(left, top, left+imgMilk.intrinsicWidth/4, top+imgMilk.intrinsicHeight/4)
             imgMilk.draw(canvas)
         }
 
-    }
+        if (imgWater != null) {
+            if (!holdingWater) {
+                if(!isDone) {
+                    left = canvasBounds.width()
+                    left /= 4
+                    xWater = left - imgWater.intrinsicWidth/4
+                    top = canvasBounds.height()
+                    yWater = top - imgWater.intrinsicHeight/4
+                } else {
+                    left = xWater+imgWater.intrinsicWidth/8
+                    top = yWater+imgWater.intrinsicHeight/8
+                }
+            } else {
+                left = xWater+imgWater.intrinsicWidth/8
+                top = yWater+imgWater.intrinsicHeight/8
+            }
+            left -= imgWater.intrinsicWidth/4
+            top -= imgWater.intrinsicHeight/4
+            imgWater.setBounds(left, top, left+imgWater.intrinsicWidth/4, top+imgWater.intrinsicHeight/4)
+            imgWater.draw(canvas)
+        }
 
-    private var holding = false
-    private var posX = 0
-    private var posY = 0
+    }
 
     override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
         when (motionEvent.action) {
-            MotionEvent.ACTION_MOVE -> {
-                if (holding) {
-                    posX = motionEvent.x.toInt()
-                    posY = motionEvent.y.toInt()
-                } else {
+            MotionEvent.ACTION_DOWN -> {
+                if(!isDone) {
                     if (imgMilk != null) {
                         if (checkClickInBB(motionEvent.x, motionEvent.y, imgMilk.bounds)) {
-                            holding = true
+                            holdingMilk = true
+                        }
+                    }
+                    if (imgWater != null) {
+                        if (checkClickInBB(motionEvent.x, motionEvent.y, imgWater.bounds)) {
+                            holdingWater = true
                         }
                     }
                 }
             }
-            MotionEvent.ACTION_UP -> holding = false
+            MotionEvent.ACTION_MOVE -> {
+                if (holdingMilk) {
+                    xMilk = motionEvent.x.toInt()
+                    yMilk = motionEvent.y.toInt()
+                } else if (holdingWater) {
+                    xWater = motionEvent.x.toInt()
+                    yWater = motionEvent.y.toInt()
+                }
+            }
+            MotionEvent.ACTION_UP -> {
+                if(holdingWater && checkClickInBB(motionEvent.x, motionEvent.y, Rect(530, 500, 600, 640))) {
+                    isDone = true
+                    xWater = motionEvent.x.toInt()
+                    yWater = motionEvent.y.toInt()
+                    textToDisplay = "Ah, ça va mieux, merci."
+                    gameView.solve()
+                }
+                if(holdingMilk && checkClickInBB(motionEvent.x, motionEvent.y, Rect(530, 500, 600, 640))) {
+                    isDone = true
+                    isFailed = true
+                    xMilk = motionEvent.x.toInt()
+                    yMilk = motionEvent.y.toInt()
+                    textToDisplay = "Oh non :("
+                    //TODO ajouter l'appel a failed
+                }
+                holdingMilk = false
+                holdingWater = false
+            }
         }
         return true
     }
