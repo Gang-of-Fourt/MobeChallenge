d58add75da08f90b2201be2acefe2415574aa87d
Maxime <maxime.fabre10@gmail.com>
2022-03-18 15:46:19 +0100

    Correction enchainement des taches

--- app/src/main/java/fr/gangoffourt/challengemobe/GameView.kt
+++ app/src/main/java/fr/gangoffourt/challengemobe/GameView.kt
@@ -8,50 +8,78 @@ import android.view.MotionEvent
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import fr.gangoffourt.challengemobe.minigame.AbstractMiniGame
-import fr.gangoffourt.challengemobe.minigame.impl.ChickGame
-import fr.gangoffourt.challengemobe.minigame.impl.TargetGame
-import fr.gangoffourt.challengemobe.minigame.impl.TestGame
+import fr.gangoffourt.challengemobe.minigame.impl.*
 import fr.gangoffourt.challengemobe.thread.ThreadGameDraw
-import fr.gangoffourt.challengemobe.thread.ThreadUpdateView
+import fr.gangoffourt.challengemobe.thread.ThreadWinDelay
 import java.lang.Exception
+import kotlin.random.Random
 
 class GameView(context: Context): SurfaceHolder.Callback, SurfaceView(context) {
 
     private var threadGameDraw = ThreadGameDraw(holder, this)
-    private var threadUpdateView = ThreadUpdateView(holder,this)
-    private var miniGameList = mutableListOf<AbstractMiniGame>()
+    private var threadWinDelay = ThreadWinDelay(holder,this)
+    private var indexArray = 0..3
+    private val currentMinigameList = mutableListOf<AbstractMiniGame>()
+    private val timeList = mutableListOf<Long>()
+    private var currentIndex = 0
+    private var seed = Random.nextLong()
+    private val random = Random(seed)
 
     init{
         holder.addCallback(this)
-        miniGameList.add(TargetGame(this, context))
+        for (i : Int in indexArray.shuffled()) {
+            currentMinigameList.add(getMiniGame(i))
+        }
+
     }
 
-    fun update() {
-
-    }
+    fun getDayNightGame() = DayNightGame(this, context)
+    fun getLawerGame() = LawyerGame(this, context)
+    fun getTargetGame() = TargetGame(this, context)
+    fun getChickGame() = ChickGame(this, context)
 
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
         canvas.drawColor(Color.WHITE) // fond blanc
-        miniGameList[miniGameList.size-1].draw(canvas)
+        currentMinigameList[currentIndex].draw(canvas)
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
         threadGameDraw.setRunning(true)
-        //threadUpdateView.setRunning(true)
         threadGameDraw.start()
-        //threadUpdateView.start()
     }
 
     override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
     }
 
     override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
-        return miniGameList[miniGameList.size-1].onTouchEvent(motionEvent)
+        return currentMinigameList[currentIndex].onTouchEvent(motionEvent)
     }
 
     fun solve () {
-        Log.d("TEST", "solved !")
+        timeList.add(System.currentTimeMillis())
+        handler.postDelayed(threadWinDelay, 2000)
+    }
+
+    fun fail () {
+
+    }
+
+    fun nextMiniGame() {
+        currentIndex ++
+        currentMinigameList.add(getMiniGame())
+    }
+
+    fun getMiniGame(i : Int = -1) : AbstractMiniGame {
+        val rdmint = random.nextInt(4)
+        Log.d("RDM", "$rdmint")
+        return when (if (i != -1) i else rdmint) {
+            0 -> getChickGame()
+            1 -> getDayNightGame()
+            2 -> getLawerGame()
+            3 -> getTargetGame()
+            else -> getTargetGame()
+        }
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder) {
@@ -59,10 +87,7 @@ class GameView(context: Context): SurfaceHolder.Callback, SurfaceView(context) {
         while (retry){
             try{
                 threadGameDraw.setRunning(false)
-                //threadUpdateView.setRunning(false)
-
                 threadGameDraw.join()
-                //threadUpdateView.join()
                 retry = false
             }
             catch (exception: Exception){
